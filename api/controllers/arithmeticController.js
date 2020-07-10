6c624d06ca9f40fc1bcb6c1d97a167123b674ece
alwell-kevin <alwell-kevin@github.com>
2020-07-10 06:51:06 -0400

    fail tests

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,10 +11,10 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return +a + +b },
+    'add':      function(a,b) { return +a + +b +1 },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
-    'divide':   function(a,b) { return a / b },
+    'divide':   function(a,b) { return a / b -1 },
   };
 
   // Determine the operation
