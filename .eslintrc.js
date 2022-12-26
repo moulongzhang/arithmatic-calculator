aa30f3dd04b832b44de875bbcb92c5665dff6e28
parroty <parroty@users.noreply.github.com>
2022-12-26 15:38:32 +0900

    Add mocha and request for eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "mocha": true,
+        "request": true
     },
     "extends": "eslint:recommended",
     "overrides": [
