15bee78ecd3bb173de35d7a51fc496f75967f5bd
parroty <parroty@users.noreply.github.com>
2023-01-22 23:42:25 +0900

    Add request library for eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "browser": true,
         "es2021": true,
-        "mocha": true
+        "mocha": true,
+        "request": true
     },
     "extends": "eslint:recommended",
     "overrides": [
